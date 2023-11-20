fce14a3bb816ee126c43d59f1c668de4f7833e57
Marcus Kuffner <marcuskuffner3125@gmail.com>
2023-11-20 13:38:55 -0500

    machine class complete
    All IO functions successfully complete, yet to be tested

--- Assets/Scripts/Machine.cs
+++ Assets/Scripts/Machine.cs
@@ -41,15 +41,27 @@ public abstract class Machine
     //Activates when an output occurs, can be used to handle unique outcomes depending on the output location. Optional.
     public abstract void handle_output(Vector2Int output_direction, string item_type);
 
+    //Activates when an output occurs, can be used to handle unique outcomes depending on the output location. Optional.
+    //includes local output point
+    public abstract void handle_output(Vector2Int output_direction, Vector2Int output_point, string item_type);
+
 
 
     //Called when an intem is being input to this machine.
     public bool input_item(Vector2Int output_machine_coord, string item_type)
     {
 
-        if (!check_inventory_full())
+        if (check_inventory_full())
         {
+            return false;
+        }
 
+        Vector2Int input = check_input_connection(output_machine_coord);
+
+        if (input != new Vector2Int())
+        {
+            handle_input(input, item_type);
+            return true;
         }
 
         return false;
@@ -57,16 +69,45 @@ public abstract class Machine
     }
 
     //Used to output an item to a nearby machine, calls input_item method on the inputting device.
-    void output_item(Vector2Int output_direction, string item_type)
+    bool output_item(Vector2Int output_direction, string item_type)
     {
+
+        GameObject target = check_output_connnection(output_direction);
+
+        if (target == new GameObject())
+        {
+            return false; 
+        }
+
+        if (target.GetComponent<Machine>().input_item(grid_coord, item_type))
+        {
+            handle_output(output_direction, item_type);
+            return true;
+        }
+
+        return false;
 
     }
 
     //Used to output an item to a nearby machine, calls input_item method on the inputting device.
     //Output point refers to the local coord on the machine from which the output is occuring, meant for larger machines.
-    void output_item(Vector2Int output_direction, Vector2Int output_point, string item_type)
+    bool output_item(Vector2Int output_direction, Vector2Int output_point, string item_type)
     {
-        
+        GameObject target = check_output_connnection(output_direction, output_point);
+
+        if (target == new GameObject())
+        {
+            return false;
+        }
+
+        if (target.GetComponent<Machine>().input_item(grid_coord + output_point, item_type))
+        {
+            inventory[item_type] -= 1;
+            handle_output(output_direction, output_point, item_type);
+            return true;
+        }
+
+        return false;
     }
 
     //Checks if the inventory is currently full, if full return true
@@ -100,10 +141,18 @@ public abstract class Machine
 
     }
 
-    //Checks if the output direction given connects to any other gameobjects in the grid, and returns the gameobject
-    GameObject check_output_connnection(Vector2Int output_direction, Vector2Int output_point)
+    //Checks if the output direction given connects to any other gameobjects in the grid, and returns the gameobject, if empty returns empty gameobject
+    GameObject check_output_connnection(Vector2Int output_direction)
     {
-        return new GameObject();
+        return GridController.grid[grid_coord.x + output_direction.x, grid_coord.y + output_direction.y];
+    }
+
+
+    //Checks if the output direction given connects to any other gameobjects in the grid, and returns the gameobject, if empty returns empty gameobject
+    //includes local output point
+    GameObject check_output_connnection(Vector2Int output_direction, Vector2Int output_point)
+    { 
+        return GridController.grid[grid_coord.x + output_point.x + output_direction.x, grid_coord.y + output_point.y + output_direction.y];
     }
 
 
