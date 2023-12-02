79ffbd5529d0abe50ec0b48f7e5d43af8398cd37
Ray <143909370+ParticularParticle@users.noreply.github.com>
2023-12-02 15:58:05 -0500

    Camera movement implemented
    Uses rigidbody and force to move camera with keyboard wasd or arrows

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 keyInputs;
+    public float moveForce;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        keyInputs = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(keyInputs * moveForce, ForceMode2D.Force);
+    }
+}
