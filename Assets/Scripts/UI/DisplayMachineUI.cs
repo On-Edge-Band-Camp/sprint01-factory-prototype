4001e5f9eedad49dd761e6541c0488643b197f26
Slivre <66972691+Slivre@users.noreply.github.com>
2024-03-12 19:50:52 -0400

    Copied Script to scene

---
+++ Assets/Scripts/UI/DisplayMachineUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DisplayMachineUI : MonoBehaviour
+{
+    GameObject UICanvas;
+    public GameObject UIToUse;
+    GameObject MachineUI;
+
+    Machine Machine;
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Hi");
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            MachineUI.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+
+    void Start()
+    {
+        UICanvas = GameObject.Find("UICanvas");
+
+        //MachineUI = Instantiate(UIToUse, UICanvas.transform);
+        //MachineUI.transform.localScale = new Vector3(0, 0, 0);
+
+        //Machine = GetComponent<Machine>();
+        //Machine.MachineUI = MachineUI;
+        //Machine.Inventory = MachineUI.GetComponentInChildren<InventoryManager>();
+
+        //TMP_Text MachineName = MachineUI.transform.Find("Title").GetComponent<TMP_Text>();
+        //MachineName.text = Machine.gameObject.name.ToString();
+    }
+
+    void Update()
+    {
+        
+    }
+}
