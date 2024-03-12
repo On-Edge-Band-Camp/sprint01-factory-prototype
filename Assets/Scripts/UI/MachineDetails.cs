102eab22f2b3009eb5d56c0e0b8db4f51ac5676a
Slivre <66972691+Slivre@users.noreply.github.com>
2024-03-12 19:30:43 -0400

    Machine Deatils prefab

---
+++ Assets/Scripts/UI/MachineDetails.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineDetails : MonoBehaviour
+{
+    public Machine machine;
+    public List<Slot> slots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Slot newSlot = transform.GetChild(i).GetComponent<Slot>();
+            if(newSlot!=null)
+            {
+                slots.Add(newSlot);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameItem item in machine.MachineInventory.Keys)
+        {
+            Debug.Log(item);
+        }
+    }
+}
